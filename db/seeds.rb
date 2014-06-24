9c0b6b6ff48dba9e5741e2608c3012f9a5207e3c
Dan Wolfe <dwolfe2@nd.edu>
2014-06-24 10:20:17 -0400

    Copied seeds.rb from hydramata-work Gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+article = Hydramata::Work::WorkTypes::Storage.create!(identity: 'article', name_for_application_usage: 'article')
+predicate_set = Hydramata::Work::PredicateSets::Storage.create!(identity: 'required', work_type: article, presentation_sequence: 1, name_for_application_usage: 'required')
+
+def within_predicate_set_create_predicate_by_term(predicate_set, term)
+  name = "dc_#{term.gsub('#', '_')}"
+  predicate = Hydramata::Work::Predicates::Storage.new(identity: "http://purl.org/dc/terms/#{term}", name_for_application_usage: name)
+  predicate.save!
+  counter = predicate_set.predicate_presentation_sequences.count + 1
+  predicate_set.predicate_presentation_sequences.create!(presentation_sequence: counter, predicate: predicate)
+end
+
+[
+  'title',
+  'alternative',
+  'creator#author',
+  'contributor#author',
+  'identifier#issn',
+  'identifier#doi',
+  'abstract',
+  'subject',
+  'source',
+  'publisher',
+  'created',
+  'format#mimeType',
+  'bibliographicCitation',
+  'contributor#repository',
+  'relation#ispartof',
+  'temporal',
+  'spatial',
+  'language',
+  'format#extent',
+  'contributor#institution',
+  'rights#permissions',
+].each do |term|
+  within_predicate_set_create_predicate_by_term(predicate_set, term)
+end
