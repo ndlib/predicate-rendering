11a8f7b0bf5e0f1535ab0c3e3feae81ebe225c6f
Dan Wolfe <dwolfe2@nd.edu>
2014-06-24 10:14:24 -0400

    Ran 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140624141127) do
+
+  create_table "hydramata_work_predicate_presentation_sequences", force: true do |t|
+    t.integer  "predicate_set_id",      null: false
+    t.integer  "predicate_id",          null: false
+    t.integer  "presentation_sequence", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "hydramata_work_predicate_presentation_sequences", ["predicate_set_id", "predicate_id"], name: "hydramata_work_predicate_presentation_sequences_identity", unique: true
+  add_index "hydramata_work_predicate_presentation_sequences", ["predicate_set_id", "presentation_sequence"], name: "hydramata_work_predicate_presentation_sequences_presentation", unique: true
+
+  create_table "hydramata_work_predicate_sets", force: true do |t|
+    t.integer  "work_type_id",               null: false
+    t.string   "identity",                   null: false
+    t.integer  "presentation_sequence",      null: false
+    t.string   "name_for_application_usage"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "hydramata_work_predicate_sets", ["work_type_id", "identity"], name: "hydramata_work_predicate_set_identity", unique: true
+  add_index "hydramata_work_predicate_sets", ["work_type_id", "presentation_sequence"], name: "hydramata_work_predicate_set_sequence", unique: true
+
+  create_table "hydramata_work_predicates", force: true do |t|
+    t.string   "identity",                   null: false
+    t.string   "name_for_application_usage"
+    t.string   "datastream_name"
+    t.string   "value_coercer_name"
+    t.string   "value_parser_name"
+    t.string   "indexing_strategy"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "hydramata_work_types", force: true do |t|
+    t.string   "identity",                   null: false
+    t.string   "name_for_application_usage"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
