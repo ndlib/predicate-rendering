fbf330f402f78d06c5ca513eb04cc070327c60ac
Dan Wolfe <dwolfe2@nd.edu>
2014-06-26 16:06:50 -0400

    Using itemprop (Microdata) and property (RDFa) on dd tag for title.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,7 +7,7 @@ class WorksController < ApplicationController
     work = Hydramata::Work::Entity.new(work_type: 'article') do |w|
       w.properties << Property("http://purl.org/dc/terms/title", "Adventures in Dublin Core", "Metadata Rox!")
     end
-    binding.pry
+    #binding.pry
     @work = Presenter(work)
   end
 end
